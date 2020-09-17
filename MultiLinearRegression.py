4b70d5b94d32e2510e4c880c3e8474f7b6913721
Danny Phung <54776410+pmdung2011@users.noreply.github.com>
2020-09-17 17:52:38 -0500

    Update MultiLinearRegression.py

--- MultiLinearRegression.py
+++ MultiLinearRegression.py
@@ -1,28 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import style
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from mpl_toolkits.mplot3d import Axes3D
-
-
-# style.use('ggplot')
+import seaborn as sns
 
 # RETRIEVE TRAINING DATA
 df = pd.read_excel('https://archive.ics.uci.edu/ml/machine-learning-databases/concrete/compressive/Concrete_Data.xls')
 X_in = df.iloc[:, 0:8]  # Get the features
 Y_in = df.iloc[:, -1]
 
-
-
 # NORMALIZE DATA
 X_in = preprocessing.normalize(X_in, norm='l2')
 
-
 # INITIALIZE COEFFICIENTS
-learning_rate = 0.5
+learning_rate = 0.8
 iterations = 10000
 B = np.zeros(X_in.shape[1])  # Initialize values of thetas
 
@@ -31,16 +23,14 @@ B = np.zeros(X_in.shape[1])  # Initialize values of thetas
 def cost_function(X, Y, B):
     n = len(Y)
     predictions = X.dot(B).flatten()
-    sqErrors = (predictions - Y) ** 2
-    J = (1.0 / (2 * n)) * sqErrors.sum()
-    return J
-    
+    return (1.0 / (2 * n)) * ((predictions - Y) ** 2).sum()
 
 
 initial_cost = cost_function(X_in, Y_in, B)
-print("Ininital_cost", initial_cost)
+print("Initial_cost_MSE", initial_cost)
 
 
+# GRADIENT DESCENT FUNCTION
 def gradient_descent(X, Y, B_cur):
     n = len(Y)
     cost_data = []
@@ -59,18 +49,17 @@ def gradient_descent(X, Y, B_cur):
 # SPLITTING DATASET TO TRAINING AND TESTING SETS
 X_train, X_test, Y_train, Y_test = train_test_split(X_in, Y_in, test_size=0.2, random_state=0)
 
-
 b, cost_arr, iteration_nums = gradient_descent(X_train, Y_train, B)
 # print(b)
-print("Learning cost: ", cost_arr[-1])
+print("Learning cost_MSE: ", cost_arr[-1])
 
 
-# MODEL EVALUATION
+# MODEL EVALUATION ROOT MEAN SQUARED ERROR
 def r_mse(Y, Y_predict):
     return np.sqrt(sum((Y - Y_predict) ** 2) / len(Y))
 
 
-# Log file
+# SAVE PARAMETERS INTO LOG FILE
 def log_file(steps, itr, c):
     file = open("log.txt", "a")
     file.write("Learning_rate: {} \n".format(str(steps)))
@@ -81,18 +70,19 @@ def log_file(steps, itr, c):
 
 log_file(learning_rate, iterations, cost_arr[-1])
 
-
 Y_pred = X_test.dot(b)
 rmse = r_mse(Y_test, Y_pred)
-print("RMSE: ", rmse)
+print("Evaluate Model_RMSE: ", rmse)
+
 # CREATE PLOT FIGURES
-# fig, ax = plt.subplots(1)  # Cost figure
-# fig2, ax2 = plt.subplots(1)  # Data figures
-# fig2 = plt.figure()
-# ax2 = Axes3D(fig2)
-# ax2.scatter(X_in[0], X_in[1], Y_in, color='#ef1234')
-# ax.plot(iteration_nums, cost_arr)
+plt.figure(1)
 plt.plot(iteration_nums, cost_arr)
 plt.xlabel("Iterations")
 plt.ylabel("Cost")
+
+plt.figure(2)
+plt.subplot(111)
+ax1 = sns.distplot(Y_test, hist=False, color="r", label="Actual Value")
+sns.distplot(Y_pred, hist=False, color="b", label="Fitted Values", ax=ax1)
+
 plt.show()
